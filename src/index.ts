bb69b7b9f165c39d84ec9ed416468b990a10bc9c
Chao Xu <fardream@users.noreply.github.com>
2022-03-27 16:35:00 +0000

    working single endpoint forwarding

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 import fetch from "cross-fetch";
 
 async function main() {
@@ -9,9 +9,10 @@ async function main() {
   const port = 8899;
   const port_ws = port + 1;
   const forwarding_url = "http://127.0.0.1:37269";
+  const forwarding_url_ws = "ws://127.0.0.1:37270";
 
   app.post("/", async (request, response) => {
-    console.log(`getting request: ${JSON.stringify(request.body, undefined, 2)}`);
+    console.log(`rpc <- request: ${JSON.stringify(request.body, undefined, 2)}`);
     const res = await fetch(forwarding_url, {
       method: "POST",
       body: JSON.stringify(request.body),
@@ -20,33 +21,43 @@ async function main() {
       },
     });
     console.log(
-      `response status: ${res.status} ${res.statusText} ${JSON.stringify(
-        res.headers,
-        undefined,
-        2
-      )}`
+      `rpc -> response status: ${res.status} ${res.statusText} ${JSON.stringify(res.headers)}`
     );
     const res_json = await res.json();
-    console.log(`${JSON.stringify(res_json)}`);
+    console.log(`rpc -> response body: ${JSON.stringify(res_json, undefined)}`);
     response.status(res.status).send(res_json);
   });
 
   app.listen(port, () => {
-    console.log(`started at ${port}`);
+    console.log(`rpc started at ${port}`);
   });
 
-  const wss = new WebSocketServer({ port: port_ws });
+  const ws_server = new WebSocketServer({ port: port_ws }, () => {
+    console.log(`ws started at ${port_ws}`);
+  });
 
-  wss.on("connection", (ws) => {
-    ws.on("message", (data, is_binary) => {
-      if (is_binary) {
-        console.log(`${data}`);
+  ws_server.on("connection", async (ws) => {
+    const ws_client = new WebSocket(forwarding_url_ws);
+    ws.on("message", async (data) => {
+      if (ws_client.readyState !== ws_client.OPEN) {
+        await new Promise((resolve) => ws_client.on("open", resolve));
       }
-      ws.send(`your data ${data}`, (err) => {
-        if (err !== null) {
-          console.log(`error: ${err}`);
+      console.log(`ws <- message received: ${data.toString()}`);
+      ws_client.send(data, (err) => {
+        if (err !== undefined) {
+          console.log(`failed to send data: ${err}`);
         }
       });
+    });
+
+    ws_client.on("message", (data) => {
+      console.log(`ws -> message response: ${data.toString()}`);
+      ws.send(data);
+    });
+
+    ws.on("close", () => {
+      console.log("closing down the web socket forwarder");
+      ws_client.close();
     });
   });
 }
